7d137c5995581f70e5bd65dcb398140ac2047d84
cyrilletual <cyrille.tual@gmail.com>
2024-02-21 16:55:59 +0100

    fix links

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,13 +1,13 @@
 "use client";
-
 import { useNotesStore } from "@/store/notesStore";
-import { useEffect } from "react";
+import Link from "next/link";
 
+/**
+ * Renders a list of notes.
+ * @returns JSX element representing the notes list.
+ */
 export default function NotesList() {
   const list = useNotesStore((state) => state.list);
-
- 
-
 
   return (
     <>
@@ -17,10 +17,15 @@ export default function NotesList() {
           {list.length > 0 && (
             <ul className="grid lg:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
               {list.map((note) => (
-                <li key={note.id} className="bg-slate-200 hover:bg-slate-50 p-4 rounded cursor-pointer">
-                  <h3 className="text-lg font-semibold">{note.title}</h3>
-                  <p className="text-gray-700">{note.subtitle}</p>
-                </li>
+                <Link href={`/notes/${note.id}`} key={note.id}>
+                  <li
+                    key={note.id}
+                    className="bg-slate-200 hover:bg-slate-50 p-4 rounded cursor-pointer"
+                  >
+                    <h3 className="text-lg font-semibold">{note.title}</h3>
+                    <p className="text-gray-700">{note.subtitle}</p>
+                  </li>
+                </Link>
               ))}
             </ul>
           )}
@@ -29,3 +34,4 @@ export default function NotesList() {
     </>
   );
 }
+// Path: src/pages/index.tsx
